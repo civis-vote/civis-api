1d074b99c7f3b56897a792e84a322531c23baf58
Michael Victor <me@mikevictor.me>
2019-06-24 15:26:00 +0530

    feat: notification settings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_24_020145) do
+ActiveRecord::Schema.define(version: 2019_06_24_092223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2019_06_24_020145) do
     t.string "last_name"
     t.integer "city_id"
     t.datetime "last_activity_at", default: -> { "(CURRENT_DATE)::timestamp without time zone" }
+    t.jsonb "notification_settings"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
