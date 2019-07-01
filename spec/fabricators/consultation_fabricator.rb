1a4beeb454acdec56ce9b82902e04882046974ef
Michael Victor <me@mikevictor.me>
2019-07-01 12:46:55 +0530

    Add inidentation to fabricators

--- spec/fabricators/consultation_fabricator.rb
+++ spec/fabricators/consultation_fabricator.rb
@@ -1,8 +1,8 @@
 Fabricator(:consultation) do
-	title											{Faker::Movies::StarWars.quote}
-	summary										{Faker::Lorem.paragraph(50, true, 8)}
-	url												{Faker::Internet.url}
-	ministry_id								{Ministry.order('RANDOM()').first.id}
-	response_deadline					{Date.today + rand(1..50).days}
-	created_by_id							{User.order('RANDOM()').first.id}
+	title											{ Faker::Movies::StarWars.quote }
+	summary										{ Faker::Lorem.paragraph(50, true, 8) }
+	url												{ Faker::Internet.url }
+	ministry_id								{ Ministry.order('RANDOM()').first.id }
+	response_deadline					{ Date.today + rand(1..50).days }
+	created_by_id							{ User.order('RANDOM()').first.id }
 end
--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,10 +1,10 @@
 Fabricator(:user) do
-	first_name 											{Faker::Name.first_name}
-	last_name 											{Faker::Name.last_name}
-	email 													{"civis+#{Faker::Name.unique.first_name}@findemail.info"}
-	password 												{Faker::Internet.password(8) }
-  city_id   											{Location.cities.order('RANDOM()').first.id}
-  notify_for_new_consultation 		{[true, false].sample}
-  role														{User.roles.to_a.sample.first}
-  after_build											{|user, transients| user.skip_confirmation_notification!}
+	first_name 											{ Faker::Name.first_name }
+	last_name 											{ Faker::Name.last_name }
+	email 													{ "civis+#{Faker::Name.unique.first_name}@findemail.info" }
+	password 												{ Faker::Internet.password(8) }
+  city_id   											{ Location.cities.order('RANDOM()').first.id }
+  notify_for_new_consultation 		{ [true, false].sample }
+  role														{ User.roles.to_a.sample.first }
+  after_build											{ |user, transients| user.skip_confirmation_notification! }
 end
