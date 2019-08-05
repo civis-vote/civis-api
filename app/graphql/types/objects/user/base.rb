1901ae645c4626a6de7cf2c91367a0be42d9a6c8
Michael Victor <me@mikevictor.me>
2019-08-05 20:13:33 +0530

    chore(user) - move last_name to user base

--- app/graphql/types/objects/user/base.rb
+++ app/graphql/types/objects/user/base.rb
@@ -4,6 +4,7 @@ module Types
 			class Base < BaseObject
 				field :id,		Int, 		nil, null: false
 				field :first_name, 	String, 	nil, 	null: false
+				field :last_name, 	String, 	nil, 	null: false
 				field :city,	Types::Objects::Location, "City where the user is registered from.", null: true
 			end
 		end
