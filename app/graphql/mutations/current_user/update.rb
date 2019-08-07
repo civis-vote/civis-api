067fa463417a1ff44d7f1a54179bc72269edceb0
Michael Kevin Victor <me@mikevictor.me>
2019-08-07 11:50:42 +0530

    improvement: add current user update
    * improvement(user) - add phone number
    
    * chore(user) - move last_name to user base
    
    * feat - added list endpoint for consultation respoonses
    
    * improvement: add update for current user

---
+++ app/graphql/mutations/current_user/update.rb
@@ -0,0 +1,19 @@
+module Mutations
+  module CurrentUser
+    class Update < Mutations::BaseMutation
+      type Types::Objects::User::CurrentUser, null: false
+
+      argument :user, Types::Inputs::CurrentUser::Update, required: true
+
+      def resolve(user:)
+        current_user = context[:current_user]
+        current_user.update! user.to_h
+        return current_user
+      end
+
+      def self.accessible?(context)
+        context[:current_user].present?
+      end
+    end
+  end
+end
