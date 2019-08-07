dd633c039fc9702c07782a6b3edce8c3ab42ee5e
Michael Victor <me@mikevictor.me>
2019-08-07 11:49:31 +0530

    improvement: add update for current user

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -5,6 +5,7 @@ module Types
     field :auth_confirm_email, 						resolver: Mutations::Auth::ConfirmEmail
     field :consultation_create,						resolver: Mutations::Consultation::Create
     field :consultation_response_create,	resolver: Mutations::ConsultationResponse::Create
+    field :current_user_update,						resolver: Mutations::CurrentUser::Update
     field :ministry_create,								resolver: Mutations::Ministry::Create
   end
 end
