4fedbb7131f9c79a2cbea2f52f5d7d14f937acdf
Michael Victor <me@mikevictor.me>
2019-08-07 10:36:20 +0530

    feat - added list endpoint for consultation respoonses

--- app/graphql/types/objects/user/current_user.rb
+++ app/graphql/types/objects/user/current_user.rb
@@ -3,9 +3,9 @@ module Types
 		module User
 			class CurrentUser < Base
 				field :created_at,									Types::Objects::DateTime, nil, 	null: false
-				field :email,												String, 									nil,	null: fals
+				field :email,												String, 									nil,	null: false
 				field :notify_for_new_consultation, Boolean, 									nil, 	null: true
-				field :phone_number,								String,									nil, 	null: true
+				field :phone_number,								String,										nil, 	null: true
 			end
 		end
 	end
