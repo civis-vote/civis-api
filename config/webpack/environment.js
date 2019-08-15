db2cbde17ace3b6ab44ead90bd04f4c27b4ee957
Anbazhagan <anbublacky@gmail.com>
2019-08-15 22:16:06 +0530

    feat: Webpacker gempath changes

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -7,7 +7,8 @@ environment.plugins.append('Provide',
   })
 )
 const { add_paths_to_environment } = require(
-  `${environment.plugins.get('Environment').defaultValues["PWD"]}/config/environments/_add_gem_paths`
+  // `${environment.plugins.get('Environment').defaultValues["PWD"]}/config/environments/_add_gem_paths`
+  `/tmp/_add_gem_paths`
 )
 
 add_paths_to_environment(environment)
