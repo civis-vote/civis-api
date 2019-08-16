92e3a7f5c96edd600b4d3275022009eaf84cdc73
Michael Victor <me@mikevictor.me>
2019-08-16 08:14:28 +0530

    improvement: add fields for ranks

--- app/graphql/types/objects/user/base.rb
+++ app/graphql/types/objects/user/base.rb
@@ -4,11 +4,14 @@ module Types
 			class Base < BaseObject
 				field :id,								Integer, 										nil, null: false
 				field :city,							Types::Objects::Location, 	"City where the user is registered from.", null: true
+				field :city_rank,					Integer,										"City rank in the system", null: false
 				field :first_name, 				String, 										nil, null: false
 				field :points,						Float,											nil, null: false
 				field :profile_picture,		Types::Objects::Attachment, nil, null: false do
 					argument :resolution, String, required: false, default_value: nil
 				end
+				field :rank,							Integer,										"Overall rank in the system", null: false
+				field :state_rank,				Integer,										"State rank in the system", null: false
 
 				def profile_picture(resolution:)
 					object.resize(resolution, "profile_picture")
