cfa623e9c18062bf20c6004bd31bed073da66313
Michael Victor <me@mikevictor.me>
2019-08-17 12:31:08 +0530

    feat: user list

---
+++ app/graphql/types/enums/user_roles.rb
@@ -0,0 +1,4 @@
+class Types::Enums::UserRoles < Types::BaseEnum
+  value "admin"
+  value "citizen"
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_16_055713) do
+ActiveRecord::Schema.define(version: 2019_08_17_065932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -175,7 +175,7 @@ ActiveRecord::Schema.define(version: 2019_08_16_055713) do
     t.integer "city_id"
     t.datetime "last_activity_at", default: -> { "(CURRENT_DATE)::timestamp without time zone" }
     t.jsonb "notification_settings"
-    t.integer "role"
+    t.integer "role", default: 0
     t.string "phone_number"
     t.string "provider"
     t.string "uid"
