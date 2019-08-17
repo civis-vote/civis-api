25687b0ac27b1b8fe467212011328e40784a83a2
Michael Victor <me@mikevictor.me>
2019-08-17 16:11:46 +0530

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -174,7 +174,7 @@ ActiveRecord::Schema.define(version: 2019_08_17_065932) do
     t.string "first_name"
     t.string "last_name"
     t.integer "city_id"
-    t.datetime "last_activity_at", default: -> { "(('now'::text)::date)::timestamp without time zone" }
+    t.datetime "last_activity_at", default: -> { "(CURRENT_DATE)::timestamp without time zone" }
     t.jsonb "notification_settings"
     t.integer "role", default: 0
     t.string "phone_number"
