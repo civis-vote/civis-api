1ed4c990acc8f5b906203647ed8837e40dc5b1b8
Michael Victor <me@mikevictor.me>
2019-08-18 14:47:46 +0530

    fix: enum spelling mistake in satisfaction ratings

--- app/graphql/types/enums/consultation_response_satisfaction_ratings.rb
+++ app/graphql/types/enums/consultation_response_satisfaction_ratings.rb
@@ -1,6 +1,6 @@
 class Types::Enums::ConsultationResponseSatisfactionRatings < Types::BaseEnum
   value "dissatisfied"
   value "somewhat_dissatisfied"
-  value "somewhat_satisified"
+  value "somewhat_satisfied"
   value "satisfied"
 end
