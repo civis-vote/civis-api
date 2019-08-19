a427389585a1c7c13b28e826410ec7dbddc960f4
Anbazhagan <anbublacky@gmail.com>
2019-08-19 20:11:23 +0530

    feat: Font awesome moved from dev to dependencies

--- db/schema.rb
+++ db/schema.rb
@@ -79,7 +79,7 @@ ActiveRecord::Schema.define(version: 2019_08_17_131929) do
   end
 
   create_table "consultations", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.string "url"
     t.datetime "response_deadline"
     t.bigint "ministry_id", null: false
@@ -175,7 +175,7 @@ ActiveRecord::Schema.define(version: 2019_08_17_131929) do
     t.string "first_name"
     t.string "last_name"
     t.integer "city_id"
-    t.datetime "last_activity_at", default: -> { "(CURRENT_DATE)::timestamp without time zone" }
+    t.datetime "last_activity_at", default: -> { "(('now'::text)::date)::timestamp without time zone" }
     t.jsonb "notification_settings"
     t.integer "role", default: 0
     t.string "phone_number"
