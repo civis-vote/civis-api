e0007984ae3d88f29ff9894f38e622a4baa66fc1
BALAJI-BALA <balaji.d.94.it@gmail.com>
2019-08-23 11:58:11 +0530

    improvements: travis fix

--- .rollbar.sh
+++ .rollbar.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 function start_deployment() {
-  deploy_id="$(curl --request POST --url https://api.rollbar.com/api/1/deploy/ --data '{"access_token": "'"$2"'", "environment": "'"${DEPLOYENV}"'", "revision": "'"${TRAVIS_COMMIT}"'", "comment": "Deployment started in '"${DEPLOYENV}"' for '"${APPLICATION_NAME}"'", "status": "'"$1"'", "local_username": "'"${AUTHOR_NAME}"'"}' | python -c 'import json, sys; obj = json.load(sys.stdin); print obj["data"]["deploy_id"]')"
+  deploy_id="$(curl --request POST --url https://api.rollbar.com/api/1/deploy/ --data '{"access_token": "'"$ROLLBAR_TOKEN"'", "environment": "'"${DEPLOYENV}"'", "revision": "'"${TRAVIS_COMMIT}"'", "comment": "Deployment started in '"${DEPLOYENV}"' for '"${APPLICATION_NAME}"'", "status": "'"$1"'", "local_username": "'"${AUTHOR_NAME}"'"}' | python -c 'import json, sys; obj = json.load(sys.stdin); print obj["data"]["deploy_id"]')"
   echo "$deploy_id" >> ~/deploy_id_from_rollbar
 }
 
 function set_deployment_success() {
-  curl --request PATCH --url "https://api.rollbar.com/api/1/deploy/${DEPLOY_ID}?access_token=$2" --data '{"status": "'"$1"'"}'
+  curl --request PATCH --url "https://api.rollbar.com/api/1/deploy/${DEPLOY_ID}?access_token=$ROLLBAR_TOKEN" --data '{"status": "'"$1"'"}'
 }
 
 if [ "$1" == "started" ]
