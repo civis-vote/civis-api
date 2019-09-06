ad6568380987eba2a9853b917be908ec63e8def7
BALAJI-BALA <balaji.d.94.it@gmail.com>
2019-09-06 15:21:46 +0530

    improvements: mutation types ordered alphabetical and expired published consultations based on deadline

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,14 +1,14 @@
 module Types
   class MutationType < Types::BaseObject
-    field :auth_sign_up,									resolver: Mutations::Auth::SignUp
-    field :auth_login,										resolver: Mutations::Auth::Login
-    field :auth_confirm_email, 						resolver: Mutations::Auth::ConfirmEmail
-    field :consultation_create,						resolver: Mutations::Consultation::Create
-    field :consultation_response_create,	        resolver: Mutations::ConsultationResponse::Create
+    field :auth_change_password,                    resolver: Mutations::Auth::ChangePassword
+    field :auth_confirm_email,                      resolver: Mutations::Auth::ConfirmEmail
+    field :auth_login,                              resolver: Mutations::Auth::Login
+    field :auth_sign_up,                            resolver: Mutations::Auth::SignUp
+    field :consultation_create,                     resolver: Mutations::Consultation::Create
+    field :consultation_response_create,            resolver: Mutations::ConsultationResponse::Create
     field :current_user_update,                     resolver: Mutations::CurrentUser::Update
-    field :auth_change_password,			        resolver: Mutations::Auth::ChangePassword
-    field :game_action_create,						resolver: Mutations::GameAction::Create
-    field :ministry_create,								resolver: Mutations::Ministry::Create
-    field :vote_create,										resolver: Mutations::ConsultationResponseVote::Create
+    field :game_action_create,                      resolver: Mutations::GameAction::Create
+    field :ministry_create,                         resolver: Mutations::Ministry::Create
+    field :vote_create,                             resolver: Mutations::ConsultationResponseVote::Create
   end
 end
