2aa66f542c0145b9388d9ac806062c5d5cd77e40
BALAJI-BALA <balaji.d.94.it@gmail.com>
2019-09-09 16:18:17 +0530

    improvements: Added flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     Current.user = current_user
     unless user_signed_in? && (current_user.admin? || current_user.moderator?)
       sign_out current_user
-      redirect_to root_path
+      redirect_to root_path, flash_info: "You need to be a admin or moderator to sign in, Please contact administrator to continue"
     end
   end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < Devise::SessionsController
 			if user.admin? || user.moderator?
 				super
 			else
-				redirect_to new_user_session_path, flash: { notice: "You need to be a admin to sign in, Please contact administrator to continue" }
+				redirect_to new_user_session_path, flash_info: "You need to be a admin or moderator to sign in, Please contact administrator to continue"
 			end
 		else
 			super
