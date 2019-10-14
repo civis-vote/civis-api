5ec48b1397e5f39a814e0d77c8654da72a7fb3d8
Michael Victor <me@mikevictor.me>
2019-10-14 12:07:04 +0530

    fix: added begin rescue for jobs that call all users

--- app/jobs/notify_new_consultation_email_job.rb
+++ app/jobs/notify_new_consultation_email_job.rb
@@ -3,7 +3,11 @@ class NotifyNewConsultationEmailJob < ApplicationJob
 
   def perform(consultation)
   	::User.notify_for_new_consultation_filter.each do |user|
-    	UserMailer.notify_new_consultation_email(user, consultation).deliver_now
+      begin
+    	 UserMailer.notify_new_consultation_email(user, consultation).deliver_now
+      rescue
+        puts "Failed to deliver email for User - #{user.id}"
+      end
     end
   end
 end
