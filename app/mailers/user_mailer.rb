0749bd4d4e74ae027f0663400fb09c2c768def41
BALAJI-BALA <balaji.d.94.it@gmail.com>
2020-01-14 17:34:05 +0530

    improvements: suggested name change for user export for exlx file

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,5 @@
 class UserMailer < ApplicationMailer
+	require 'axlsx'
 
 	def verify_email(user)
 		@@postmark_client.deliver_with_template(from: 'Civis'+ unless Rails.env.production? then +' - ' + Rails.env.titleize else '' end  + '<support@civis.vote>',
@@ -23,6 +24,17 @@ class UserMailer < ApplicationMailer
 																							ministry_name: consultation.ministry.name,
 																							feedback_url: consultation.feedback_url,
 																							unsubscribe_url: user.unsubscribe_url
+																						})
+	end
+
+	def notify_new_consultation_email_to_admin(user, consultation)
+		@@postmark_client.deliver_with_template(from: 'Civis'+ unless Rails.env.production? then +' - ' + Rails.env.titleize else '' end  + '<support@civis.vote>',
+																						to: user.email,
+																						template_alias: "notify-new-consultation-to-admin",
+																						template_model:{
+																							consultation_name: consultation.title,
+																							deadline: consultation.response_deadline.strftime('%e-%m-%Y'),
+																							review_url: consultation.review_url
 																						})
 	end
 
@@ -73,5 +85,95 @@ class UserMailer < ApplicationMailer
 																								url: client_url,
 																								unsubscribe_url: user.unsubscribe_url
 																							})
+	end
+
+	def consultation_export_email_job(consultations, email)
+		size_arr = []
+    consultations.size.times { size_arr << 22 }
+		excel_file = "#{Dir.tmpdir()}/consultations-sheet_#{Time.now.to_s}.xlsx"
+		file_name = "consultations-sheet_#{Time.now.to_s}.xlsx"
+		xlsx = Axlsx::Package.new
+		xlsx.workbook.add_worksheet(name: "Consultations") do |sheet|
+		  sheet.add_row ["Title", "Url", "Response Deadline", "Ministry", "Status", "Summary", "Response Count", "Featured", "Reading Time", "Created At"], b: true
+		  consultations.each do |consultation|
+		    sheet.add_row [consultation.title, consultation.url, consultation.response_deadline, consultation.ministry.name, consultation.status, consultation.summary.to_plain_text, consultation.consultation_responses_count, consultation.is_featured, consultation.reading_time, consultation.created_at]
+		  end
+			sheet.column_widths *size_arr
+		end
+    xlsx.serialize(excel_file)
+    user = User.find_by(email: email)
+    file = File.open(excel_file)
+		@@postmark_client.deliver_with_template(from: 'Civis'+ unless Rails.env.production? then +' - ' + Rails.env.titleize else '' end  + '<support@civis.vote>',
+																						to: user.email,
+																						template_id: 13651891,
+																						template_model:{
+																							first_name: user.first_name
+																						},
+                                              attachments: [{
+                                                name: file_name,
+                                                content: [file.read].pack('m'),
+                                                content_type: 'application/vnd.ms-excel'
+                                              }]
+                                            )
+	end
+
+	def consultation_responses_export_email_job(consultation_responses, email)
+		size_arr = []
+    consultation_responses.size.times { size_arr << 22 }
+		excel_file = "#{Dir.tmpdir()}/consultation-responses-sheet_#{Time.now.to_s}.xlsx"
+		file_name = "consultations-responses-sheet_#{Time.now.to_s}.xlsx"
+		xlsx = Axlsx::Package.new
+		xlsx.workbook.add_worksheet(name: "Consultation Responses") do |sheet|
+		  sheet.add_row ["Consultation Title", "Consultation Response Text", "Submitted By", "Satisfication Rating", "Visibility", "Submitted At"], b: true
+		  consultation_responses.each do |consultation_response|
+		    sheet.add_row [consultation_response.consultation.title, consultation_response.response_text, consultation_response.user.full_name, consultation_response.satisfaction_rating, consultation_response.visibility, consultation_response.created_at]
+		  end
+			sheet.column_widths *size_arr
+		end
+    xlsx.serialize(excel_file)
+    user = User.find_by(email: email)
+    file = File.open(excel_file)
+		@@postmark_client.deliver_with_template(from: 'Civis'+ unless Rails.env.production? then +' - ' + Rails.env.titleize else '' end  + '<support@civis.vote>',
+																						to: user.email,
+																						template_id: 13651891,
+																						template_model:{
+																							first_name: user.first_name
+																						},
+                                              attachments: [{
+                                                name: file_name,
+                                                content: [file.read].pack('m'),
+                                                content_type: 'application/vnd.ms-excel'
+                                              }]
+                                            )
+	end
+
+	def user_export_email_job(users, email)
+		size_arr = []
+    users.size.times { size_arr << 22 }
+		excel_file = "#{Dir.tmpdir()}/users-sheet_#{Time.now.to_s}.xlsx"
+		file_name = "users-sheet_#{Time.now.to_s}.xlsx"
+		xlsx = Axlsx::Package.new
+		xlsx.workbook.add_worksheet(name: "Users") do |sheet|
+		  sheet.add_row ["First Name", "Last Name", "Email", "Points", "Rank", "Best Rank", "Best Rank Type", "State Rank", "City", "City Type", "Subscribed", "Role", "Phone Number"], b: true
+		  users.each do |user|
+		    sheet.add_row [user.presented?("first_name"), user.presented?("last_name"), user.email, user.presented?("points"), user.presented?("rank"), user.presented?("best_rank"), user.presented?("best_rank_type"), user.presented?("state_rank"), user.city.present? ? user.city.name : 'NA', user.city.present? ? user.city.location_type : 'NA', user.presented?("created_at"), user.notify_for_new_consultation.present? ? 'true' : 'false', user.role, user.presented?("phone_number")]
+		  end
+			sheet.column_widths *size_arr
+		end
+    xlsx.serialize(excel_file)
+    user = User.find_by(email: email)
+    file = File.open(excel_file)
+		@@postmark_client.deliver_with_template(from: 'Civis'+ unless Rails.env.production? then +' - ' + Rails.env.titleize else '' end  + '<support@civis.vote>',
+																						to: user.email,
+																						template_id: 13651891,
+																						template_model:{
+																							first_name: user.first_name
+																						},
+                                              attachments: [{
+                                                name: file_name,
+                                                content: [file.read].pack('m'),
+                                                content_type: 'application/vnd.ms-excel'
+                                              }]
+                                            )
 	end
 end
