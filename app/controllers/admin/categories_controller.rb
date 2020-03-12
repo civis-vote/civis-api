07c6268ec365d1aca0502a02458918b08b96c2a6
BALAJI-BALA <balaji.d.94.it@gmail.com>
2020-03-12 16:15:57 +0530

    improvements: category search added to admin panel

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -60,7 +60,7 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def filter_params
-    params.require(:filters).permit(:search, :status_filter) if params[:filters]
+    params.require(:filters).permit(:search_query, :status_filter) if params[:filters]
   end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -23,6 +23,11 @@ class Category < ApplicationRecord
     end
   end
 
+  scope :search_query, lambda { |query = nil|
+    return nil unless query
+    where("name ILIKE (?)", "%#{query}%")
+  }
+
   scope :sort_records, lambda { |sort, sort_direction = "asc"|
     return nil if sort.blank?
     order("#{sort} #{sort_direction}")
