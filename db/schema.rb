b0b5dd4fe0dc282fc8434b90e0bdc1b071a4ea4e
BALAJI-BALA <balaji.d.94.it@gmail.com>
2020-05-29 16:59:43 +0530

    feat: Added organization, callbackUrl, designation fields were added to user object

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_101310) do
+ActiveRecord::Schema.define(version: 2020_05_29_102333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,25 @@ ActiveRecord::Schema.define(version: 2020_05_28_101310) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "cover_photo_data"
     t.jsonb "cover_photo_versions_data"
+  end
+
+  create_table "cm_page_builder_rails_page_components", force: :cascade do |t|
+    t.string "uuid", null: false
+    t.bigint "page_id", null: false
+    t.string "content"
+    t.integer "position"
+    t.string "component_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["page_id"], name: "index_cm_page_builder_rails_page_components_on_page_id"
+  end
+
+  create_table "cm_page_builder_rails_pages", force: :cascade do |t|
+    t.string "container_type", null: false
+    t.bigint "container_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["container_type", "container_id"], name: "container_composite_index"
   end
 
   create_table "constants", force: :cascade do |t|
@@ -237,6 +256,9 @@ ActiveRecord::Schema.define(version: 2020_05_28_101310) do
     t.uuid "uuid", default: -> { "uuid_generate_v4()" }, null: false
     t.text "profile_picture_data"
     t.jsonb "profile_picture_versions_data"
+    t.string "organization"
+    t.string "callback_url"
+    t.string "designation"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -245,6 +267,7 @@ ActiveRecord::Schema.define(version: 2020_05_28_101310) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "api_keys", "users"
+  add_foreign_key "cm_page_builder_rails_page_components", "cm_page_builder_rails_pages", column: "page_id"
   add_foreign_key "consultation_response_votes", "consultation_responses"
   add_foreign_key "consultation_response_votes", "users"
   add_foreign_key "consultation_responses", "consultations"
