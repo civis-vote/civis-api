04af7c8f1d2de6906fc70c6bc1ea835fd027caa7
BALAJI-BALA <balaji.d.94.it@gmail.com>
2020-05-29 23:18:59 +0530

    improvements: removed unwanted exposed fields from current user types

--- app/graphql/types/inputs/current_user/update.rb
+++ app/graphql/types/inputs/current_user/update.rb
@@ -3,12 +3,9 @@ module Types
 		module CurrentUser
 			class Update < Types::BaseInputObject
 				graphql_name "CurrentUserUpdateInput"
-				argument :callback_url,									String,		nil,	required: false
 				argument :city_id,											Integer,	nil,	required: false
-				argument :designation,									String,		nil,	required: false
 				argument :first_name,										String,		nil,	required: false
 				argument :last_name,										String,		nil,	required: false
-				argument :organization, 								String,		nil,	required: false
 				argument :phone_number,									String,		nil,	required: false
 				argument :profile_picture_file,					Types::Inputs::Attachment, nil, required: false
 			end
