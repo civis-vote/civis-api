ac10be3d22e2acf260b96034cd81a595bd002514
BALAJI-BALA <balaji.d.94.it@gmail.com>
2020-06-01 09:39:31 +0530

    feat: added designation and organization field in current user type

--- app/graphql/types/inputs/current_user/update.rb
+++ app/graphql/types/inputs/current_user/update.rb
@@ -4,8 +4,10 @@ module Types
 			class Update < Types::BaseInputObject
 				graphql_name "CurrentUserUpdateInput"
 				argument :city_id,											Integer,	nil,	required: false
+				argument :designation,									String,		nil,	required: false
 				argument :first_name,										String,		nil,	required: false
 				argument :last_name,										String,		nil,	required: false
+				argument :organization, 								String,		nil,	required: false
 				argument :phone_number,									String,		nil,	required: false
 				argument :profile_picture_file,					Types::Inputs::Attachment, nil, required: false
 			end
