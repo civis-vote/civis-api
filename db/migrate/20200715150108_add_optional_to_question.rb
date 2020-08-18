ebb6c93ef5d9f13c0ee4a7ca769c2e9add1f721d
BALAJI-BALA <balaji.d.94.it@gmail.com>
2020-08-18 13:47:35 +0530

    improvements: removed consultation summary field from the create form

--- db/migrate/20200715150108_add_optional_to_question.rb
+++ db/migrate/20200715150108_add_optional_to_question.rb
@@ -1,5 +1,8 @@
 class AddOptionalToQuestion < ActiveRecord::Migration[6.0]
-  def change
+  def self.up
     add_column :questions, :is_optional, :boolean, default: false
+  end
+  def self.down
+  	remove_column :questions, :optional, :boolean, default: false
   end
 end
