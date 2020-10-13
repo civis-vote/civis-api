51de4d8ca809788df5b5d335f09dc0d7d98b852f
BALAJI-BALA <balaji.d.94.it@gmail.com>
2020-10-13 16:48:38 +0530

    feat: Added last_updated_at field to the user export excel

--- app/graphql/queries/stats/impact.rb
+++ app/graphql/queries/stats/impact.rb
@@ -8,10 +8,12 @@ module Queries
 	    	consultation_count = ::Consultation.where(status: [:expired, :published]).count
 	    	consultation_response_count = ::ConsultationResponse.count
 	    	consultation_response_distinct_users = ::ConsultationResponse.distinct.pluck(:user_id).count
+	    	registered_users = ::User.all.size
 	    	return {
 	    		consultation_count: consultation_count,
 	    		consultation_response_count: consultation_response_count,
 	    		consultation_response_distinct_users: consultation_response_distinct_users,
+	    		total_registered_users: registered_users
 	    	}
 	    end
 		end
