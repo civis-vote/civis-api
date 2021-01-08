feecfc5012922fd996ba4bd05680800ddc157f47
BALAJI-BALA <balaji.d.94.it@gmail.com>
2021-01-09 00:36:10 +0530

    improvements: Seeds file updated regarding to the latest db changes

--- app/models/concerns/scorable/consultation.rb
+++ app/models/concerns/scorable/consultation.rb
@@ -8,7 +8,7 @@ module Scorable
 	    before_save :add_consultation_created_points, if: :status_changed? if respond_to? :before_save
 
 	    def add_consultation_created_points
-	    	self.created_by.add_points(:consultation_created) if ( self.status_changed?(from: "submitted", to: "published") && self.public_consultation? )
+	    	self.created_by.add_points(:consultation_created) if ( self.status_changed?(from: "submitted", to: "published") && self.public_consultation? && !self.created_by.admin? && self.created_by.city.present?)
 	    end
 	  end
   end
