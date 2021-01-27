bb2d60a4fab8e3e7fa753b27eb99c7271b8b71d7
BALAJI-BALA <balaji.d.94.it@gmail.com>
2021-01-27 18:06:31 +0530

    improvements: Added points for submitting response after the location update

--- app/models/concerns/scorable/consultation_response.rb
+++ app/models/concerns/scorable/consultation_response.rb
@@ -6,17 +6,18 @@ module Scorable
     included do 
 
 	    after_commit :add_response_created_points, on: :create if respond_to? :after_commit
-
 	    def add_response_created_points
-	    	points_to_add = 0.0
-	    	point_event = self.user.add_points(:response_submitted)
-	    	points_to_add += point_event.points
-	    	if self.shared?
-	    		point_event = self.user.add_points(:public_response_submitted)
-	    		points_to_add += point_event.points
-	    	end
-	    	self.update(points: points_to_add)
-	    	self.template.user.add_points(:response_used) if self.template.present?
+	    	if !self.user.admin? && self.user.city.present?
+		    	points_to_add = 0.0
+		    	point_event = self.user.add_points(:response_submitted)
+		    	points_to_add += point_event.points
+		    	if self.shared?
+		    		point_event = self.user.add_points(:public_response_submitted)
+		    		points_to_add += point_event.points
+		    	end
+		    	self.update(points: points_to_add)
+		    	self.template.user.add_points(:response_used) if self.template.present?
+		    end
 	    end
 	  end
   end
