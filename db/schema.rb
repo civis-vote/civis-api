3f005ba339e46648c0dd4512cb7a3f8815190bc7
Rohit Choudhary <31502136+rohit0147@users.noreply.github.com>
2021-05-27 13:06:05 +0530

    updated schema version to avoid merge conflict
    feature/profanity automatic merge was failing due to this

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_25_161427) do
+ActiveRecord::Schema.define(version: 2021_05_05_194649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
