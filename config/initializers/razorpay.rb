2c459da7c629fb782f2587d6c12495a48bf63ac1
Yash Trivedi <yash.m.trivedi@tsgforce.com>
2021-06-07 08:33:28 +0000

    donate updated credentials file

--- config/initializers/razorpay.rb
+++ config/initializers/razorpay.rb
@@ -1,5 +1,5 @@
 require "razorpay"
 
-# key_id = Rails.application.credentials.dig(:razorpay, :key_id)
-# secret_key = Rails.application.credentials.dig(:razorpay, :secret_key)
-Razorpay.setup('rzp_test_7uFpRekBxdblL5', 'Uabud32eND8nHlqhNxu0mUN2')
+key_id = Rails.application.credentials.dig(:razorpay, :key_id)
+secret_key = Rails.application.credentials.dig(:razorpay, :secret_key)
+Razorpay.setup(key_id, secret_key)
