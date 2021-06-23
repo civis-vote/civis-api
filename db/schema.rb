804789c6637c9be6ae2a4e7685c57780e4189d86
Rohit Choudhary <31502136+rohit0147@users.noreply.github.com>
2021-06-23 13:55:24 +0530

    updated schema rb to avoid merge conflict changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 2021_06_04_090823) do
+ActiveRecord::Schema.define(version: 2021_06_23_080517) do
 
 
   # These are extensions that must be enabled in order to support this database
