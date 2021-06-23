bd27f7ce1f63a0de9670770a69a7dad396a8aa8d
BALAJI-BALA <balaji.d.94.it@gmail.com>
2021-06-23 19:28:58 +0530

    improvements: resetted coloumn info to change db type (#609)
    * improvements: modified the code to accept response for the questions and answers from string
    
    * improvements: allowed multiple option from checkbox question type in response import
    
    * improvements: added guest user name for displaying off-platform users name
    
    * improvements: Added user name or guest name in the response object
    
    * Update app/graphql/types/objects/consultation_response/base.rb
    
    Co-authored-by: Michael Kevin Victor <me@mikevictor.me>
    
    * improvements: resetted coloumn info to change db type
    
    Co-authored-by: Michael Kevin Victor <me@mikevictor.me>

--- db/migrate/20210604090823_change_integer_to_consultation_response.rb
+++ db/migrate/20210604090823_change_integer_to_consultation_response.rb
@@ -1,5 +1,6 @@
 class ChangeIntegerToConsultationResponse < ActiveRecord::Migration[6.0]
   def change
+  	ConsultationResponse.reset_column_information
   	change_column :consultation_responses, :import_key, :string
   end
 end
