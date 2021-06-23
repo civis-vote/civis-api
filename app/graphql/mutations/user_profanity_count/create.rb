c718f284aa8ded27923997d77692fe41e6ec035a
rohit0147 <rohitchoudhary0147@gmail.com>
2021-06-23 13:20:02 +0530

    added profanity operations

---
+++ app/graphql/mutations/user_profanity_count/create.rb
@@ -0,0 +1,16 @@
+module Mutations
+    module UserProfanityCount
+      class Create < Mutations::BaseMutation
+      type Types::Objects::UserProfanityCount::Base, null: false
+      argument :user_profanity_count, Types::Inputs::UserProfanityCount::Create, required: true
+      def resolve(user_profanity_count:)
+        newProfaneCountMapping = ::UserProfanityCount.new user_profanity_count.to_h
+        newProfaneCountMapping.user_id = user_profanity_count.user_id
+        newProfaneCountMapping.profanity_count = user_profanity_count.profanity_count 
+        newProfaneCountMapping.save!
+        return newProfaneCountMapping
+        end
+      end
+    end
+  end
+  
---
+++ app/graphql/mutations/user_profanity_count/update.rb
@@ -0,0 +1,15 @@
+module Mutations
+    module UserProfanityCount
+    class Update < Mutations::BaseMutation
+        type Types::Objects::UserProfanityCount::Base, null: false
+  
+      argument :user_profanity_count, Types::Inputs::UserProfanityCount::Create, required: true
+      def resolve(user_profanity_count:)
+        currentProfaneCountMapping = ::UserProfanityCount.find_by(user_id:user_profanity_count.user_id)
+        currentProfaneCountMapping.update!(profanity_count: user_profanity_count[:profanity_count])
+        return currentProfaneCountMapping
+        end
+      end
+    end
+  end
+  
