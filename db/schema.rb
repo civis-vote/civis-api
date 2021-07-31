300eb666ee97308557d1cfc6b628477ebb592d22
Raj <mrajk5@gmail.com>
2021-07-31 10:43:00 +0000

    Updated schema db with unique index on user_counts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_25_071952) do
+ActiveRecord::Schema.define(version: 2021_07_31_071952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -305,7 +305,7 @@ ActiveRecord::Schema.define(version: 2021_07_25_071952) do
     t.integer "short_response_count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_user_counts_on_user_id"
+    t.index ["user_id"], name: "index_user_counts_on_user_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
