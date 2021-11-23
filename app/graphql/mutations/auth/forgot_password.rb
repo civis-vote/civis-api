be120b7c1d5463d729fe5dbcaf319d4f962cc149
“sedhumadhavan19” <sedhumadhavanit@gmail.com>
2021-11-23 19:15:51 +0530

    chore: forgot password response changes

--- app/graphql/mutations/auth/forgot_password.rb
+++ app/graphql/mutations/auth/forgot_password.rb
@@ -15,7 +15,7 @@ module Mutations
         forgot_password_url = user.forgot_password_url(raw_token)
         ForgotPasswordEmailJob.perform_later(user, forgot_password_url)
         user.find_or_generate_api_key
-        return "If you account exists, you will receive an email with password reset instructions. If you haven’t received the email, please contact support for further help."
+        return "If your account exists, you will receive an email on your registered email address, with the instructions to rephrase the password. If you haven’t received the email, please contact info@civis.vote for support"
       end
     end
   end
