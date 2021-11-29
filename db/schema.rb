a93eacf4d17b46f00413cad3f2d8452440858863
sedhu madhav <sedhumadhavanit@gmail.com>
2021-11-29 00:38:50 -0800

    Revert "develop"

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_032403) do
-
+ActiveRecord::Schema.define(version: 2021_11_20_150340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -281,15 +280,6 @@ ActiveRecord::Schema.define(version: 2021_11_22_032403) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "round_number"
     t.index ["consultation_id"], name: "index_response_rounds_on_consultation_id"
-  end
-
-  create_table "sessions", force: :cascade do |t|
-    t.string "session_id", null: false
-    t.text "data"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
-    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
