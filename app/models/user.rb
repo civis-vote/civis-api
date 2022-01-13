4cd7b893a2596932a4972b5f72cc8796fc4ea733
Manikandan <manikandan0603@gmail.com>
2022-01-13 16:42:27 +0530

    fix: password complexity regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -192,7 +192,7 @@ class User < ApplicationRecord
   end
 
   def password_complexity
-    if password.present? && !password.match("^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&]).{8,}$")
+    if password.present? && !password.match(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&]).{8,}$/)
       errors.add :password, "Password length min 8 charcter and include at least one alphabet, one special character, and one digit"
     end
   end
