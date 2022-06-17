9b06402d723bd3738781c12d01e44135a39f69ed
Jishnu De Sarkar <desarkarjishnu@gmail.com>
2022-06-17 14:34:57 +0530

    FEAT: Added cors validation

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins "*"
-
-    resource "*", headers: :any, methods: :any
+  allow do 
+    origins 'localhost:[0-9]+',
+    '127.0.0.1:[0-9]+',
+    'https://*.civis.vote:[0-9]+'
+    resource '*',
+    methods: [:get, :post, :delete, :put, :patch, :options, :head],
+    headers: :any,
+    max_age: 600
   end
 end
