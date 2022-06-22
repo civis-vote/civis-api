d2973a7d9fc186fa01816ce7c1755c0fbee26881
Jishnu De Sarkar <desarkarjishnu@gmail.com>
2022-06-22 19:02:40 +0530

    FIX: Whitelist origins due to cors error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do 
-    origins Rails.application.config.allowed_cors_origins
+    origins '*'
     resource '*',
     methods: [:get, :post, :delete, :put, :patch, :options, :head],
     headers: :any,
