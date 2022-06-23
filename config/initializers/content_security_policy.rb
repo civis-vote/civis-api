10dadf6ee5e5af31804e056f97bf0ce0a8c49274
Jishnu De Sarkar <desarkarjishnu@gmail.com>
2022-06-23 16:00:54 +0530

    HOTFIX: Admin panel show pages not opening due to inline CSP violation

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,9 +1,9 @@
 Rails.application.config.content_security_policy do |policy|
   policy.default_src :none
-  policy.font_src    :self
-  policy.img_src     :self
+  policy.font_src    :self, :data, :https
+  policy.img_src     :self, :data
   policy.object_src  :none
-  policy.script_src  :self
-  policy.style_src   :self
+  policy.script_src  :self, :unsafe_inline
+  policy.style_src   :self, :https, :unsafe_inline
   policy.report_uri "/csp-violation-report-endpoint"
 end
