1b8f8551a6cceb029e4197b8e6a7530e36a12316
Aditya Tiwari <taditya.tiwari007@gmail.com>
2023-08-11 19:20:00 +0530

    Chore: Cosmatic changes

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -3,8 +3,10 @@ require "shrine/storage/file_system"
 require "shrine/storage/s3"
 
 test_secret_key = "036b0c1d57dc1e57e369c1441ae13c44231dad82df902a03420456b8c0ec3e3b5d1c53ff27fa6651307f13bda811cd5e0c4227c07fa906e499ad967bbd0a019f"
-Shrine.plugin :activerecord           # loads Active Record integration
-Shrine.plugin :cached_attachment_data # enables retaining cached file across form redisplays
+# loads Active Record integration
+Shrine.plugin :activerecord
+# enables retaining cached file across form redisplays
+Shrine.plugin :cached_attachment_data
 Shrine.plugin :restore_cached_data
 Shrine.plugin :remote_url, max_size: nil
 Shrine.plugin :derivation_endpoint, secret_key: Rails.env.test? ? test_secret_key : Rails.application.credentials.dig(:secret_key_base)
@@ -16,7 +18,7 @@ def production_storages
 
   {
     cache: Shrine::Storage::FileSystem.new(Rails.root.join("public"), prefix: "uploads/cache"),
-    store: Shrine::Storage::S3.new(prefix: 'uploads', upload_options: { acl: 'public-read' }, **s3_options)
+    store: Shrine::Storage::S3.new(prefix: "uploads", upload_options: { acl: "public-read" }, **s3_options),
   }
 end
 
@@ -27,14 +29,16 @@ def staging_storages
 
   {
     cache: Shrine::Storage::FileSystem.new(Rails.root.join("public"), prefix: "uploads/cache"),
-    store: Shrine::Storage::S3.new(prefix: 'uploads', upload_options: { acl: 'public-read' }, **s3_options)
+    store: Shrine::Storage::S3.new(prefix: "uploads", upload_options: { acl: "public-read" }, **s3_options),
   }
 end
 
 def development_storages
   {
-    cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), #temporary
-    store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads') # permanent
+    # temporary
+    cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),
+    # permanent
+    store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),
   }
 end
 
