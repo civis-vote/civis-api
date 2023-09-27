7bb8d678654726ef2d85f8f37d2b2409ed8fb907
Aditya Tiwari <taditya.tiwari007@gmail.com>
2023-09-27 21:01:49 +0530

    Chore: Change indentation to spaces

--- app/jobs/notify_pending_review_of_profane_responses_email_to_admin_job.rb
+++ app/jobs/notify_pending_review_of_profane_responses_email_to_admin_job.rb
@@ -2,9 +2,9 @@ class NotifyPendingReviewOfProfaneResponsesEmailToAdminJob < ApplicationJob
   queue_as :default
 
   def perform(consultation)
-  	::User.where(role: [:admin]).each do |user|
+    ::User.where(role: [:admin]).each do |user|
       begin
-    	 UserMailer.notify_pending_review_of_profane_responses_email_to_admin(user, consultation).deliver_now
+        UserMailer.notify_pending_review_of_profane_responses_email_to_admin(user, consultation).deliver_now
       rescue
         puts "Failed to deliver email for User - #{user.id}"
       end
