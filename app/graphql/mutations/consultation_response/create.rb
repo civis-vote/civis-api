b2cdb2b811a579bcbaff6dca1ab5550d504a08fa
Aditya Tiwari <taditya.tiwari007@gmail.com>
2024-03-13 17:06:10 +0530

    Chore: Store the current user into a variable

--- app/graphql/mutations/consultation_response/create.rb
+++ app/graphql/mutations/consultation_response/create.rb
@@ -6,14 +6,15 @@ module Mutations
       argument :consultation_response, Types::Inputs::ConsultationResponse::Create, required: true
 
       def resolve(consultation_response:)
+        user = context[:current_user]
         created_consultation_response = ::ConsultationResponse.new consultation_response.to_h
-        created_consultation_response.user = context[:current_user]
+        created_consultation_response.user = user
         @consultation = ::Consultation.find(consultation_response.consultation_id)
         active_response_round = @consultation.response_rounds.order(:created_at).last
         created_consultation_response.response_round = active_response_round
         if @consultation.private_consultation?
           raise GraphQL::ExecutionError, "Private response is enforced, response visibility can't be shared" if (@consultation.private_response? && created_consultation_response.visibility == "shared")
-          respondent = ::Respondent.find_by(user: context[:current_user], response_round: active_response_round)
+          respondent = ::Respondent.find_by(user: user, response_round: active_response_round)
           created_consultation_response.respondent_id = respondent.id if respondent
         end
         created_consultation_response.save!
