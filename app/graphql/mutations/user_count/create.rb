55cd7b5b5f87dfd5df6a4e42652e5c0236366e6d
Austin Jacob <austinjb32@gmail.com>
2025-05-16 09:59:36 +0530

    Updated API Issue in Civis

--- app/graphql/mutations/user_count/create.rb
+++ app/graphql/mutations/user_count/create.rb
@@ -3,13 +3,20 @@ module Mutations
     class Create < Mutations::BaseMutation
       type Types::Objects::UserCount::Base, null: false
       argument :user_count, Types::Inputs::UserCount::Create, required: true
+
       def resolve(user_count:)
-        newCountMapping = ::UserCount.new user_count.to_h
-        newCountMapping.user_id = user_count.user_id
-        newCountMapping.profanity_count = user_count.profanity_count
-        newCountMapping.short_response_count = user_count.short_response_count
-        newCountMapping.save!
-        return newCountMapping
+        new_count_mapping = ::UserCount.find_or_create_by(user_id: user_count.user_id) do |record|
+          record.profanity_count = user_count.profanity_count
+          record.short_response_count = user_count.short_response_count
+        end
+
+        # Update the fields if the record already exists
+        new_count_mapping.update!(
+          profanity_count: user_count.profanity_count,
+          short_response_count: user_count.short_response_count
+        )
+
+        new_count_mapping
       end
     end
   end
