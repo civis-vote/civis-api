5e4ef929a18d34fe555ac21bbea2c6f4eab02a29
Austin Jacob <austinjb32@gmail.com>
2025-05-22 19:47:29 +0530

    Updated the Error Text in verify otp

--- app/graphql/mutations/auth/verify_otp.rb
+++ app/graphql/mutations/auth/verify_otp.rb
@@ -7,7 +7,7 @@ module Mutations
 
       def resolve(auth:)
         user = ::User.find_by(email: auth[:email])
-        raise FailedLogin, "Incorrect OTP" unless user&.verify_otp(auth[:otp])
+        raise FailedLogin, "You have entered a wrong OTP" unless user&.verify_otp(auth[:otp])
 
         user.find_or_generate_api_key
         user.live_api_key
