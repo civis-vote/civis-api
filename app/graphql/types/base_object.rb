03017fafcdedb01d7272d5e72aac621a6b3ac387
Mahaveer Soni <68422113+Mahaveer1141@users.noreply.github.com>
2025-06-24 10:41:44 +0530

    fix: nil

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -1,7 +1,7 @@
 module Types
   class BaseObject < GraphQL::Schema::Object
     def attachment_with_resolution(attachment_name, resolution)
-      width, height = resolution.split("x").map(&:to_i)
+      width, height = resolution&.split("x")&.map(&:to_i)
       attachment = object.public_send(attachment_name)
 
       return unless attachment.attached?
