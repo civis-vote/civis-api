4e7c8e954c8b5e894e8f1f8c34b771a1a89a20de
Mahaveer Soni <68422113+Mahaveer1141@users.noreply.github.com>
2025-07-04 16:39:33 +0530

    fix: use cm role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,20 @@
 class ApplicationController < ActionController::Base
-	protect_from_forgery with: :null_session
-	add_flash_types :flash_info, :flash_success_info
+  protect_from_forgery with: :null_session
+  add_flash_types :flash_info, :flash_success_info
   include ActiveStorage::SetCurrent
-  
 
   def require_admin
     Current.user = current_user
     return if user_signed_in? && (current_user.admin? || current_user.moderator?)
+
     sign_out current_user
-    redirect_to root_path, flash_info: "You need to be a admin or moderator or organisation employee to sign in, Please contact administrator to continue"
+    redirect_to root_path,
+                flash_info: "You need to be a admin or moderator or organisation employee to sign in, Please contact administrator to continue"
   end
 
   def after_sign_in_path_for(resource)
     if resource.admin? || resource.moderator?
-    	admin_users_path
+      admin_users_path
     elsif resource.organisation_employee?
       organisation_setting_path(resource.organisation_id)
     else
@@ -23,8 +24,12 @@ class ApplicationController < ActionController::Base
 
   def require_organisation_employee
     Current.user = current_user
-    return if user_signed_in? && current_user.organisation_employee? && session[:organisation_id] == current_user.organisation_id && current_user.organisation.active?
+    if user_signed_in? && current_user.organisation_employee? && session[:organisation_id] == current_user.organisation_id && current_user.organisation.active?
+      return
+    end
+
     sign_out current_user
-    redirect_to root_path, flash_info: "You need to be an admin or moderator or organisation employee to sign in, Please contact administrator to continue"
+    redirect_to root_path,
+                flash_info: "You need to be an admin or moderator or organisation employee to sign in, Please contact administrator to continue"
   end
 end
--- app/models/concerns/scorable/user.rb
+++ app/models/concerns/scorable/user.rb
@@ -3,21 +3,20 @@ module Scorable
     extend ActiveSupport::Concern
     include Scorable
 
-    included do 
-
-	    after_commit :add_user_created_points, on: :create if respond_to? :after_commit
+    included do
+      after_commit :add_user_created_points, on: :create if respond_to? :after_commit
       after_commit :add_user_created_points, if: :saved_change_to_city_id?
-	    def add_user_created_points
-	    	self.add_points(:user_created)
-        point_events = self.point_events.joins(:point_scale).where(point_scales: {action: "response_submitted"})
-        consultation_responses = self.responses
-        if !point_events.present? && consultation_responses.present?
-          consultation_responses.each do | consultation_response|
-            consultation_response.add_response_created_points
-          end
+      def add_user_created_points
+        add_points(:user_created)
+        point_events = self.point_events.joins(:point_scale).where(point_scales: { action: "response_submitted" })
+        consultation_responses = responses
+        return unless !point_events.present? && consultation_responses.present?
+
+        consultation_responses.each do |consultation_response|
+          consultation_response.add_response_created_points
         end
-	    end
-	  end
+      end
+    end
 
     class << self
       def update_national_rank
@@ -29,6 +28,7 @@ module Scorable
 
       def update_state_rank(state)
         return false unless state.present?
+
         state_users = ::User.citizen.where(city_id: state.child_ids)
         distinct_points = state_users.distinct(:points).pluck(:points).sort.reverse
         distinct_points.each_with_index do |point_value, index|
@@ -43,34 +43,34 @@ module Scorable
           city_users.where(points: point_value).update_all(city_rank: index + 1)
         end
       end
-    end    
+    end
 
     def add_points(action)
-    	point_scale = calculate_point_scale(action)
-    	point_event = self.point_events.create(point_scale: point_scale, points: point_scale.points)
-      if !self.admin? && self.city.present?
+      point_scale = calculate_point_scale(action)
+      point_event = point_events.create(point_scale: point_scale, points: point_scale.points)
+      if !role?('admin') && city.present?
         User.update_national_rank
-        User.update_state_rank(self.city.parent)
+        User.update_state_rank(city.parent)
         User.update_city_rank(city)
-        self.check_for_new_best_rank
+        check_for_new_best_rank
       end
-      return point_event
+      point_event
     end
 
     def calculate_point_scale(action)
-    	if PointScale.where("upper_limit > ?", self.points).where(action: action).order(upper_limit: :asc).present?
-    		point_scale = PointScale.where("upper_limit > ?", self.points).where(action: action).order(upper_limit: :asc).first
-    		return point_scale
-    	else
-    		puts "Could not find point scale!"
-    	end
+      if PointScale.where("upper_limit > ?", points).where(action: action).order(upper_limit: :asc).present?
+        PointScale.where("upper_limit > ?", points).where(action: action).order(upper_limit: :asc).first
+
+      else
+        puts "Could not find point scale!"
+      end
     end
 
     def check_for_new_best_rank
-      self.update(best_rank: self.city_rank, best_rank_type: :city) unless self.best_rank.present?
-      self.update(best_rank: self.city_rank, best_rank_type: :city) if self.city_rank.present? && self.best_rank > self.city_rank
-      self.update(best_rank: self.state_rank, best_rank_type: :state) if self.state_rank.present? && self.best_rank > self.state_rank
-      self.update(best_rank: self.rank, best_rank_type: :national) if self.rank.present? && self.best_rank > self.rank   
+      update(best_rank: city_rank, best_rank_type: :city) unless best_rank.present?
+      update(best_rank: city_rank, best_rank_type: :city) if city_rank.present? && best_rank > city_rank
+      update(best_rank: state_rank, best_rank_type: :state) if state_rank.present? && best_rank > state_rank
+      update(best_rank: rank, best_rank_type: :national) if rank.present? && best_rank > rank
     end
   end
 end
