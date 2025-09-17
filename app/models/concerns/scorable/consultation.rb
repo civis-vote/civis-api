d792b64fb8b1d928ec78fc3dd84e2ae126557fa2
Mahaveer Soni <68422113+Mahaveer1141@users.noreply.github.com>
2025-09-17 18:16:29 +0530

    feat: add Super Admin role and validate ministry logo file type

--- app/models/concerns/scorable/consultation.rb
+++ app/models/concerns/scorable/consultation.rb
@@ -6,8 +6,8 @@ module Scorable
       before_save :add_consultation_created_points, if: :status_changed? if respond_to? :before_save
 
       def add_consultation_created_points
-        if status_changed?(from: "submitted", to: "published") && public_consultation? &&
-           !created_by.role?('admin') && created_by.city.present?
+        if status_changed?(from: "submitted", to: "published") && public_consultation? && created_by.city.present? &&
+           %w[admin super_admin].exclude?(created_by.cm_role&.name&.parameterize&.underscore.to_s)
           created_by.add_points(:consultation_created)
         end
       end
