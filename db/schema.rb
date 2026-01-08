88407f03c8d1188949c466ca64fa682552e5797c
Sanchita Singh <sanchitasingh3099@gmail.com>
2026-01-09 01:32:10 +0530

    Ability to manage the team members section

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_10_111312) do
+ActiveRecord::Schema[7.1].define(version: 2026_01_08_171452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gist"
   enable_extension "plpgsql"
@@ -495,6 +495,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_10_111312) do
     t.datetime "updated_at", null: false
     t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
     t.index ["updated_at"], name: "index_sessions_on_updated_at"
+  end
+
+  create_table "team_members", force: :cascade do |t|
+    t.string "name"
+    t.string "designation"
+    t.boolean "active"
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "themes", force: :cascade do |t|
