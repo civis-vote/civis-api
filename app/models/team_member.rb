074b58668b6845a18b18183aa072c2d6b22c1108
Sanchita Singh <sanchitasingh3099@gmail.com>
2026-01-19 18:04:38 +0530

    civ-5: user profile and svg image issue fixed

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -10,6 +10,7 @@ class TeamMember < ApplicationRecord
 
   validates :name, :designation, presence: true
   validates :profile_picture, presence: true
+  validate :validate_profile_picture_extension
 
   scope :alphabetical, -> {
     order(Arel.sql("LOWER(name) ASC"))
@@ -17,6 +18,15 @@ class TeamMember < ApplicationRecord
 
   def status
     active ? :active : :inactive
+  end
+
+  private
+
+  def validate_profile_picture_extension
+    return unless profile_picture.attached?
+    return if %w[image/jpeg image/png image/jpg].include?(profile_picture.content_type)
+
+    errors.add(:profile_picture, 'must be a JPG or PNG image')
   end
 
 end
