67014f9432c6fc79502b6af1445e285e2d9517bb
SAN3099 <59970068+SAN3099@users.noreply.github.com>
2026-01-22 14:14:54 +0530

    Apply suggestion from @mikevic
    Co-authored-by: Michael Kevin Victor <me@mikevictor.me>

--- app/models/concerns/cm_admin/department.rb
+++ app/models/concerns/cm_admin/department.rb
@@ -19,7 +19,7 @@ module CmAdmin
           page_title 'Departments'
 
           filter %i[name], :search, placeholder: 'Search'
-          filter :status, :single_select, collection: [%w[Approved true], ['Not Approved', false]],filter_with: :status_filter
+          filter :status, :single_select, collection: [%w[Approved true], ['Not Approved', false]], filter_with: :status_filter
           filter :name, :multi_select, helper_method: :select_options_for_department, filter_with: :name_filter, active_by_default: true
           filter :level, :multi_select, active_by_default: true
           filter :location_id, :multi_select, helper_method: :select_options_for_location, active_by_default: true
