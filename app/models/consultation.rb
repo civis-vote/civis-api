ca33c79674e03bce34af517ece2bc759b091d682
Austin Jacob <austinjb32@gmail.com>
2026-08-28 15:40:10 +0530

    feat(consultation): add viewable_by scope for user org and public non-org consultations

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -123,6 +123,19 @@ class Consultation < ApplicationRecord
 
   scope :organisation_only, -> { where(organisation_id: Current.user&.organisation_id) }
 
+  scope :viewable_by, lambda { |user = nil|
+    user ||= Current.user
+
+    return none if user.blank?
+
+    if user.organisation_id.present?
+      where(organisation_id: user.organisation_id)
+        .or(where(visibility: :public_consultation, organisation_id: nil))
+    else
+      where(visibility: :public_consultation, organisation_id: nil)
+    end
+  }
+
   def notify_admins
     self.response_token = SecureRandom.uuid unless response_token
     save!
